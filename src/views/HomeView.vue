79a8bde3855bb7c595334d10b56142c26a549177
Vinicius Souza <vinicius.souza@nre.tec.br>
2023-02-10 09:21:12 -0300

    :construction: working on home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home-view">
-    <h1 class="text-center">Testes</h1>
+  <div class="home-view container">
+    <div class="row  py-5 justify-content-center">
+      <div class="col-lg-10">
+        <div class="row">
+          <div class="col-lg-6 order-lg-2">
+            <h2>Welcome to the Pokedex</h2>
+          </div>
+          <div class="col-lg-6 order-lg-1">
+            <img src="@/assets/imgs/home-image.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
